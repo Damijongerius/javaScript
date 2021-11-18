a38c254f9062e08e27b7c2f2606ac5937030a107
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:17:50 +0100

    op1tm5 js

---
+++ sectie2/op1tm5/index.js
@@ -0,0 +1,19 @@
+const x=1;
+const y = "1";
+const z = 2;
+
+//opdracht1
+console.log(x+y-2);
+
+//opdracht2
+console.log(x+Number(y)-2);
+
+
+//opdracht3
+console.log(z+y-x);
+
+//opdracht4
+console.log(x+String(z)+y);
+
+//opdracht5
+console.log(x+"44");
