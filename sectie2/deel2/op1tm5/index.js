23456fc92e4223d137e42690c9435345ba507df7
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:13:25 +0100

    op1tm5 js
    nani

---
+++ sectie2/deel2/op1tm5/index.js
@@ -0,0 +1,18 @@
+const FirstName = "dami";
+const LastName = "jongerius";
+const Age = 15;
+
+console.log(FirstName+" "+ LastName+" "+ Age)
+///////////////////////////////////////////////
+//opdracht2
+console.log("Mijn naam is" + FirstName);
+///////////////////////////////////////////////
+//opdracht3
+console.log("mijn volledige naam is" + FirstName+" "+ LastName)
+///////////////////////////////////////////////
+//opdracht4
+console.log("in 2056 ben ik "+ (Age + 2056-2021) )
+///////////////////////////////////////////////
+//opdracht5
+console.log("mijn naam is "+FirstName+" en ik ben "+Age+" jaar oud. Daarnaast is mijn famillie naam "+LastName)
+
