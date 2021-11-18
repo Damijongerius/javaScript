ed8092701f3ad546fa567f3dce6594ca24bc6d21
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:18:17 +0100

    op6tm9 js

---
+++ sectie2/op6tm9/index.js
@@ -0,0 +1,43 @@
+//opdracht 6 
+//opdracht 7
+// opdracht 8
+
+const weather = "??";
+
+
+switch(weather){
+    case("regen"):
+    console.log("ik neem een paraplu mee naar buiten");
+    break;
+    case("zon"):
+    console.log("ik neem een zonnebril mee")
+    break;
+    default:
+    console.log("ik neem mijn goede humeur mee")
+}
+
+//opdracht 9
+
+const Day = "maandag"
+
+switch(Day){
+    case("maandag"):
+    case("dinsdag"):
+    console.log("ik ga boksen");
+    break;
+    case("donderdag"):
+    case("vrijdag"):
+    console.log("BJJ")
+    break;
+    case("woensdag"):
+    case("zaterdag"):
+    case("zondag"):
+    console.log("thuis eten");
+    break;
+    default:
+    console.log("volgens mij is dit geen dag of je hebt het fout getyped");
+
+}
+
+
+
