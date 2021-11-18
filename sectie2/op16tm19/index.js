72b845d4cdbe799855a0742677c685321439693d
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 21:54:27 +0100

    Update index.js

---
+++ sectie2/op16tm19/index.js
@@ -0,0 +1,32 @@
+//opdracht 16
+const age = 18;
+const Studie = true;
+
+if(age==18 && Studie == true){
+    console.log("je hebt recht op studiefinanciering");
+}else{
+    console.log("helaas je hebt geen recht op studiefinanciering");
+}
+
+//opdracht 17
+const ouders = 2; //ouders waarmee zei contacht heeft
+const verzogersHuis = true;
+
+if(age==18 && Studie == true && ouders == 2){
+    console.log("je hebt recht op studiefinanciering");
+    if(verzorgersHuis == true){
+        console.log("daarboven krijg je korting omdat je bij je ouders woont")
+    }
+}else{
+    console.log("helaas je hebt geen recht op studiefinanciering");
+
+}
+
+//opdracht 18
+/*
+ik ben cool dus ik heb gelijk en ik denk dat het goed is idk
+
+*/
+
+//opdracht 19
+//ja ze krijgt studiefinanciering
