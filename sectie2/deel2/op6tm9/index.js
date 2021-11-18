25c72c2c6955b2e0b8f5f7d1b58e5b5d24e5698c
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:15:09 +0100

    op6tm9 js
    nani

---
+++ sectie2/deel2/op6tm9/index.js
@@ -0,0 +1,37 @@
+const regen = true;
+
+if(regen == true){
+    console.log("ik doe mijn paraplu open");
+}else{
+    console.log("ik heb geen paraplu nodig nu ")
+}
+////////////////////////////////////////////////
+// opdracht 7
+const grasKleur = "geel";
+
+if(grasKleur == "geel"){
+  console.log("ik moet het gras even water geven")  
+}else{
+    console.log("het grass heeft nog geen water nodig")
+}
+
+///////////////////////////////////////////////////
+//opdracht 8
+const seizoen = "winter";
+
+if(seizoen == "Winter"){
+    console.log("ik zet de verwarming aan")
+}else{
+    console.log("ik ga dansen op de tafel")
+}
+////////////////////////////////////////////////////
+//opdracht 9
+const HardeRegen = false;
+
+if(regen== true && HardeRegen == true){
+    console.log("ik doe mijn paraplu open en regenjas aan");
+}else if(regen&&!HardeRegen){
+    console.log("ik doe mijn paraplu open")
+}else{
+    console.log("ik doe mijn zonnebril op")
+}
