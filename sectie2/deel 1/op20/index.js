d4644859894d2d1fc1f2400e06a9120636b7221f
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:09:37 +0100

    op20
    nani

---
+++ sectie2/deel 1/op20/index.js
@@ -0,0 +1,10 @@
+//stel je voor het kind is een jongen
+
+const lengte = [183 , 178];
+const gender = "jongen"
+
+if(gender== "jongen"){
+    console.log((lengte[1]*3)+lengte[2]/4)
+}else{
+    console.log((lengte[2]*3)+lengte[1]/4)
+}
