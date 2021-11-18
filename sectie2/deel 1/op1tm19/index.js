5fbe591f278c3544f92eb0b2c1caecfffffa98cf
Damijongerius <87931528+Damijongerius@users.noreply.github.com>
2021-11-18 09:09:25 +0100

    op 1tm19
    nani

---
+++ sectie2/deel 1/op1tm19/index.js
@@ -0,0 +1,151 @@
+// opdracht 1
+console.log("0pdracht1");
+const basis = 8;
+const middel = 6;
+const high = 7;
+
+console.log(basis);
+console.log(middel);
+console.log(high);
+/////////////////////////////////////////////////////////////////////////
+//opdracht 2
+console.log("0pdracht2");
+const age = 15;
+
+console.log(age);
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 3
+console.log("0pdracht3");
+
+let firstName = "herder;"
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 4
+console.log("0pdracht4");
+
+firstName = 13;
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 5
+console.log("0pdracht5");
+
+firstName = "konijn";
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 6
+console.log("0pdracht6");
+
+firstName = true;
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 7
+console.log("0pdracht7");
+
+firstName = "13";
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 8
+console.log("0pdracht8");
+
+firstName = "1984";
+console.log(typeof(firstName))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 9
+console.log("0pdracht9");
+
+const lastName = "Schmitz";
+firstName = "Jorn";
+console.log(firstName + lastName)
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 10
+console.log("0pdracht10");
+
+const rabbit = "konijn"
+console.log(typeof(rabbit))
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 11 , 12 , 13
+console.log("0pdracht11,12,13");
+let birthYear = 2006;
+let year = 2021;
+
+let myAge = year - birthYear
+console.log(myAge) 
+console.log(myAge*2)
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 14
+console.log("0pdracht14");
+
+let grondgetal = 1;
+let exponent = 5;
+
+while(exponent > 0){
+    grondgetal=grondgetal*10;
+    exponent--;
+}
+console.log(grondgetal)
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 15
+console.log("0pdracht15");
+
+let street= "oak";
+let number= 291;
+
+console.log(street+" "+number)
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 16
+console.log("0pdracht16");
+
+ NumberOne = 12;
+
+console.log(NumberOne+15);
+
+NumberOne + 15;
+
+console.log(NumberOne);
+
+//////////////////////////////////////////////////////////////////////////
+//opdracht 17
+console.log("0pdracht17");
+
+NumberOne = 12;
+
+console.log(NumberOne/15);
+
+NumberOne/15;
+
+console.log(NumberOne);
+//////////////////////////////////////////////////////////////////////////
+//opdracht 18
+console.log("0pdracht18");
+
+NumberOne = 12;
+
+console.log(NumberOne+1);
+
+NumberOne++;
+
+console.log(NumberOne);
+//////////////////////////////////////////////////////////////////////////
+//opdracht 19
+console.log("0pdracht19");
+
+NumberOne = 12;
+
+console.log(NumberOne-1);
+
+NumberOne--;
+
+console.log(NumberOne);
+
